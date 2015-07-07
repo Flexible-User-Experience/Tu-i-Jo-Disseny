087f55f4e59e8ba7e3711d6a3856276b8ec6110d
David Romaní <david@flux.cat>
2015-07-07 17:59:33 +0200

    fix some sensio labs insights issues

--- src/AppBundle/Entity/Traits/ImageTrait.php
+++ src/AppBundle/Entity/Traits/ImageTrait.php
@@ -2,11 +2,7 @@
 
 namespace AppBundle\Entity\Traits;
 
-use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Component\HttpFoundation\File\File;
-use Symfony\Component\Validator\Constraints as Assert;
-use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * Class ImageTrait
