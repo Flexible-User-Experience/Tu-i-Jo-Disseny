0a01977d925b5de794de4451d2bb7ff1683293f2
David Romaní <david@flux.cat>
2016-04-07 20:50:49 +0200

    improve frontend homepage view 8

---
+++ app/DoctrineMigrations/Version20160407204735.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160407204735 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE contact_message (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, phone VARCHAR(255) DEFAULT NULL, message TEXT NOT NULL, checked TINYINT(1) NOT NULL, answered TINYINT(1) NOT NULL, answer TEXT DEFAULT NULL, position INT NOT NULL, enabled TINYINT(1) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, removed_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE contact_message');
+    }
+}
--- src/AppBundle/Controller/Front/FrontendController.php
+++ src/AppBundle/Controller/Front/FrontendController.php
@@ -2,9 +2,13 @@
 
 namespace AppBundle\Controller\Front;
 
+use AppBundle\Entity\ContactMessage;
+use AppBundle\Form\Type\ContactMessageType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class FrontendController
@@ -18,17 +22,29 @@ class FrontendController extends Controller
     /**
      * @Route("/", name="front_homepage")
      * @Method({"GET"})
+     * @param Request $request
+     *
+     * @return Response
      */
-    public function homepageAction()
+    public function homepageAction(Request $request)
     {
         $projects = $this->getDoctrine()->getRepository('AppBundle:Project')->findAllEnabledAndShowInHomepageSortedByPosition();
         $services = $this->getDoctrine()->getRepository('AppBundle:Service')->findAllEnabledSortedByPosition();
+        $contact = new ContactMessage();
+        $form = $this->createForm(ContactMessageType::class, $contact);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+        }
+
 
         return $this->render(
             '::Front/homepage.html.twig',
             [
-                'projects' => $projects,
-                'services' => $services,
+                'projects'     => $projects,
+                'services'     => $services,
+                'contact_form' => $form->createView(),
             ]
         );
     }
