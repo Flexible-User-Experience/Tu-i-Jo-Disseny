8695f8521a6a2d64d679aa7aa3392118365c2f1a
David Romaní <david@flux.cat>
2016-11-02 11:27:04 +0100

    improve frontend posts detail view 2

--- src/AppBundle/Controller/Front/BlogController.php
+++ src/AppBundle/Controller/Front/BlogController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller\Front;
 
+use AppBundle\Entity\BlogPost;
+use Doctrine\ORM\EntityNotFoundException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -32,15 +34,29 @@ class BlogController extends Controller
     /**
      * @Route("/blog/{year}/{month}/{day}/{slug}", name="front_blog_posts_detail")
      *
+     * @param $year
+     * @param $month
+     * @param $day
      * @param $slug
      *
      * @return Response
+     * @throws EntityNotFoundException
      */
     public function projectAction($year, $month, $day, $slug)
     {
+        $date = \DateTime::createFromFormat('Y-m-d', $year . '-' . $month . '-' . $day);
+
+        /** @var BlogPost $post */
         $post = $this->getDoctrine()->getRepository('AppBundle:BlogPost')->findOneBy(
-            [ 'slug' => $slug ]
+            [
+                'publishedAt' => $date,
+                'slug'        => $slug,
+            ]
         );
+
+        if (!$post) {
+            throw new EntityNotFoundException();
+        }
 
         return $this->render(
             ':Front/blog:detail.html.twig',
